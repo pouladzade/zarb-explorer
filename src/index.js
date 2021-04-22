476e6f69c1f405f375fa0121673ebb566cf1a520
NAGARAJ <nagaraj@NAGARAJs-MBP.realtek>
2021-04-22 13:27:38 +0800

    changes

--- src/index.js
+++
@@ -1,7 +0,0 @@
-require('file-loader?name=[name].[ext]!./index.html')
-import React from 'react';
-import ReactDom from 'react-dom';
-import App from './App'
-
-const appElement = document.getElementById('app');
-ReactDom.render(<App/>,appElement);
